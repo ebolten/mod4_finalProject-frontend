ebbb1a8be26f97465a4bd9c72110559360ee7858
keithburgie <krburgie@gmail.com>
2019-07-26 11:59:39 -0400

    Fix errors from Game to Stage change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './styles/App.scss';
-import Game from './components/Game'
+import Stage from './components/Stage'
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-        <Game />
+        <Stage />
       </div>
     );
   }
