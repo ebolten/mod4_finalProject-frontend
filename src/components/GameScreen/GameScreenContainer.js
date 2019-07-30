5f8123697c659b7f7eef946c77c8ed19fa437fcd
keithburgie <krburgie@gmail.com>
2019-07-30 12:11:12 -0400

    Removed commented line

--- src/components/GameScreen/GameScreenContainer.js
+++ src/components/GameScreen/GameScreenContainer.js
@@ -1,6 +1,5 @@
 // component-name-container.js is your business logic and state management as handled before being sent to the stateless view Game.
 import React, {Fragment} from 'react';
-// import styles from './Game.module.scss';
 import GameHeader from '../GameHeader'
 import PancakeContainer from '../Pancake'
 // import GameView from './GameView';
